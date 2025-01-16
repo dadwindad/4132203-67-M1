a114d521577d15a848bac78f9d2db73a60d9c810
dadwindad <vongpramate@gmail.com>
2025-01-16 10:27:28 +0700

    lab5

--- lab40/app/php/blog.php
+++ lab40/app/php/blog.php
@@ -7,6 +7,36 @@ $method = $_SERVER['REQUEST_METHOD'];
 $response = ['status' => 'error', 'message' => 'Invalid request'];
 
 switch ($method) {
+
+    case 'GET':
+        // Get blog by id
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $stmt = $conn->prepare("SELECT * FROM tb_blog WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $blog = $result->fetch_assoc();
+
+            if ($blog) {
+                $response = ['status' => 'success', 'data' => $blog];
+            } else {
+                $response = ['status' => 'error', 'message' => 'Blog not found'];
+            }
+            $stmt->close();
+        } else {
+            // Get all blogs
+            $result = $conn->query("SELECT * FROM tb_blog");
+            $blogs = [];
+
+            while ($row = $result->fetch_assoc()) {
+                $blogs[] = $row;
+            }
+
+            $response = ['status' => 'success', 'data' => $blogs];
+        }
+        break;
+
     case 'POST': // Insert new blog
         $title = $_POST['title'] ?? null;
         $post = $_POST['post'] ?? null;
